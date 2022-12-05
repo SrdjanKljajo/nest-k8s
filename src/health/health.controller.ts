ab3039d224729a31f8351aa5d3ff6d3a9780b00f
SrdjanKljajo <websmartist@gmail.com>
2022-12-05 18:00:27 +0100

    add prefix api and secure pkg

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import {
-  DiskHealthIndicator,
   HealthCheck,
   HealthCheckService,
   HttpHealthIndicator,
-  MemoryHealthIndicator,
 } from '@nestjs/terminus';
 import { PrismaHealthIndicator } from './prisma.health';
 
@@ -14,23 +12,14 @@ export class HealthController {
     private healthCheckService: HealthCheckService,
     private http: HttpHealthIndicator,
     private db: PrismaHealthIndicator,
-    private disk: DiskHealthIndicator,
-    private memory: MemoryHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   checkHealth() {
     return this.healthCheckService.check([
-      () => this.http.pingCheck('Basic Check', ''),
+      () => this.http.pingCheck('Basic Check', 'https://docs.nestjs.com'),
       () => this.db.isHealthy('Database is Connected'),
-      () =>
-        this.disk.checkStorage('diskStorage', {
-          thresholdPercent: 0.8,
-          path: 'C:\\',
-        }),
-      () => this.memory.checkHeap('memory_heap', 300 * 1024 * 1024),
-      () => this.memory.checkRSS('memory_rss', 300 * 1024 * 1024),
     ]);
   }
 }
